3947e0cf8620aa3f9dbe829420da0d4f5d9ea218
Bhavana N <96416690+BhavanaN30@users.noreply.github.com>
2022-03-07 06:49:00 +0000

    p2final.c

--- p2final.c
+++ p2final.c
@@ -4,10 +4,10 @@ struct_fraction
   int num,den;
 };
 typedef struct fraction fraction;
-fraction input_fraction;
+fraction input_fraction()
 {
   fraction f;
-  printf("enter fraction");
+  printf("enter fraction\n");
   scanf("%d %d",&f.num,&f.den);
   return f;
 }
@@ -16,4 +16,33 @@ fraction largest_fraction(fraction f1, fraction f2,fraction f3)
   int sden=f1.den*f2.den*f3
 }
 int a,b,c;
-c=f3.numerator
+a = f1.num * sden/f1.den;
+b = f2.num * sden/f2.den;
+c = f3.num * sden/f3.den;
+if(a>b && a>c)
+{
+  retutrn f1;
+}
+else if(b>c)
+{
+  return f2;
+}
+else
+{
+  return f3;
+}
+}
+void output(fraction f1,fraction f2,fraction f3,fraction largest)
+{
+  printf("The largest of the three fractions is %d/%d",largest.num,largest.den);
+}
+int main()
+{
+  fraction f1,f2,f3,largest;
+  f1=input_fraction();
+  f2=input_fraction();
+  f3=input_fraction();
+  largest=largest_fraction(f1,f2,f3);
+  output(f1,f2,f3,largest);
+  return 0;
+}
