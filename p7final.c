ff9bdf3e6e3071ddc2ee267b0e92657d543862fd
Bhavana N <96416690+BhavanaN30@users.noreply.github.com>
2022-04-01 03:35:03 +0000

    p7final.c

--- p7final.c
+++ p7final.c
@@ -1,14 +1,33 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+struct _fraction
 {
-  int a,b,c,d,x,y,i,gcd;
-  printf("\nEnter the numberator for 1st number;");
-  scanf("%d",&a);
-  printf("\nEnetr the denominator for 1st number:");
-  scanf("%d",&b);
-  printf("\nEnter the numerator for 2nd number:");
-  scanf("%d",&c);
-  printf("\nEnetr the denominator for 2nd number:");
-  scanf("%d",&d);
-  x=(a*)
+int num,den;
+};
+typedef struct _fraction fraction;
+int find_gcd(int a,int b){
+  int min=a;
+  if(b<min)
+    min=b;
+  for(int i=1;i<=min;i--){
+    if(a%i==0 && b%i==0)
+      return i;
   }
+}
+fraction input_fraction(fraction *f1,fraction *f2){
+  printf("enter the numerator and denomintor\n");
+  scanf("%d%d%d%d",&f1->num,&f1->den,&f2->num,&f2->den);  f3->den=f1.den*f2.den);
+  f3->num=f1.num*f2.den+f2.num*f1.den;
+  int gcd=find_gcd(f3->num,f3->den);
+  f3->num=f3->num/gcd;
+  f3->den=f3->den/gcd;
+}
+void output(fraction f1,fraction f2,fraction f3){
+  printf("the sum of %d/%d and %d/%d is %d/%d ",f1.num,f1.den,f2.num,f2.den,f3.num,f3.den);
+}
+int main(){
+  fraction f1,f2,f3;
+  input_fraction(&f1,&f2);
+  add_fraction(f1,f2,&f3);
+  output(f1,f2,f3);
+  return 0;
+}
